1be7a122f2a4c4cb61d8f27ff82cb306c5c1cbb6
Joseph <jigishagachuru336@gmail.com>
2026-08-02 21:44:26 +0300

    Pillars

--- src/components/Pillars.tsx
+++ src/components/Pillars.tsx
@@ -96,9 +96,7 @@ const Pillars: React.FC = () => {
       <div className="mt-12 text-center">
         <img src="/image 6.jpeg" alt="Our Pillars" className="mx-auto rounded-2xl w-xl h-xl shadow-lg" />
       </div>
-      <div className="mt-12 text-center">
-        <img src="/image 7.jpeg" alt="Our Pillars" className="mx-auto rounded-2xl w-xl h-xl shadow-lg" />
-      </div>
+      
 
       
     </section>
